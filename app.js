bef411c990b0e67b45f5eeda025a506688547fec
Mariusz <m2ario123@wp.pl>
2022-01-01 12:53:59 +0100

    Aktualizacja html style oraz app

--- app.js
+++ app.js
@@ -3,13 +3,28 @@ const inputWpiszCzynnosc = document.querySelector('.inputWpiszCzynnosc');
 const przyciskDodajCzynnosc = document.querySelector('.przyciskDodajCzynnosc');
 
 
-function dodajCzynnosc(){
-    const nowaCzynnosc = document.createElement('p');
-    nowaCzynnosc.innerHTML = inputWpiszCzynnosc.value;
-    przechowujeNowaCzynnosc.appendChild(nowaCzynnosc);
-    
-}
 
+function dodajCzynnosc(){
+    // stworzenie elementu p
+    const nowaCzynnosc = document.createElement('p');
+    // jeśli nie wpisano czynności to zwróć, inaczej dodaj 
+    if(inputWpiszCzynnosc.value === ''){
+        return;
+    }else{
+        nowaCzynnosc.innerHTML = inputWpiszCzynnosc.value;
+    }
+    // dodanie nowej czynności do pojemnika
+    przechowujeNowaCzynnosc.appendChild(nowaCzynnosc);
+    // wyczyszczenie inputu z czynnością
+    inputWpiszCzynnosc.value = '';
+    nowaCzynnosc.addEventListener('click', function(){
+        nowaCzynnosc.style.textDecoration = "line-through";
+    })
+    // Usuwanie czynności 2x clikiem
+    nowaCzynnosc.addEventListener('dblclick', function(){
+        przechowujeNowaCzynnosc.removeChild(nowaCzynnosc);
+    })   
+}
 
 
 
