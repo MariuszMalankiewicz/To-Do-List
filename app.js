8a5f69520983104b4260bb32d32284898f0c7f83
Mariusz <m2ario123@wp.pl>
2022-01-27 11:28:16 +0100

    update

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
 const button = document.querySelector("button");
 const input = document.querySelector('input');
 const ol = document.querySelector('ol');
-let span = document.querySelector('span');
+// let span = document.querySelector('span');
 
-
-let list = [];
+// let list = [];
 
 function AddToArray()
 {
     list.push(input.value);
 }
+function RemoveLi(e){
+    e.target.remove();
+}
 function AddLi()
 {
     if(input.value == ''){
-        alert('Write Task');
         return;
-    }else{
-        li = document.createElement('li');
-        li.textContent = input.value;
-        ol.appendChild(li);
-        input.value === '';
     }
+    li = document.createElement('li');
+    li.textContent = input.value;
+    ol.appendChild(li);
+    input.value = '';
+    li.addEventListener('click', RemoveLi);
 }
-function AddI()
-{
-    i = document.createElement('i');
-    i.className = "fas fa-times-circle";
-    li.appendChild(i);
-}
-function CounterTask(){
-    span.innerHTML = "(" + list.length + ")";
-}
+
+// function AddI()
+// {
+//     i = document.createElement('i');
+//     i.className = "fas fa-times-circle";
+//     li.appendChild(i);
+// }
+
+// function CounterTask(){
+    // span.innerHTML = "(" + list.length + ")";
+// }
+
 
 
 button.addEventListener('click', () =>{
     AddToArray();
     AddLi();
-    AddI();
-    CounterTask();
-    console.log(list);
+    // AddI();
+    // CounterTask();
 })
-
-        
-
